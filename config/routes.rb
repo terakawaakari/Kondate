1b1301c568c90cdcfe119016eff178882a4b61f2
terakawaakari <terakawaakari0701@gmail.com>
2021-05-08 13:39:06 +0000

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'recipes/index'
-  get 'recipes/new'
-  get 'recipes/show'
-  get 'recipes/edit'
-  get 'users/show'
+
+  root to: 'homes#top'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
+
+  resources :recipes
+
+  resources :ingredients
+
 end
