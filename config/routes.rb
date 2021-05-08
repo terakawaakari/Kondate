b00b023ff1f0423e1e38a204ccfd8a48fd06f270
terakawaakari <terakawaakari0701@gmail.com>
2021-05-08 13:27:17 +0000

    コントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/new'
+  get 'recipes/show'
+  get 'recipes/edit'
+  get 'users/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
